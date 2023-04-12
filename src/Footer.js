514422c019eae1168cba2ad811870d8881b8d2b4
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-04-12 11:20:54 +0100

    added footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer className="footer">Adastral Fish & Chips</footer>;
+  return (
+    <footer className="footer">
+      This website was redesigned and rebuilt for{" "}
+      <a
+        href="https://www.adastralfishnchips.co.uk/"
+        target="_blank"
+        rel="noreferrer"
+        title="Adastral Fish & Chips"
+      >
+        Adastral Fish & Chips
+      </a>{" "}
+      by
+      <a
+        href="https://www.ontaito.com/"
+        target="_blank"
+        rel="noreferrer"
+        title="On-Tai To's page"
+      >
+        {" "}
+        On-Tai To{" "}
+      </a>
+      . <br />
+      Please refer to the latest information by calling the shop directly.
+    </footer>
+  );
 }
