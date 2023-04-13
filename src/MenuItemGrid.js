fd29a7077c3fdd449a65288344702045781ea4ad
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-04-13 14:07:02 +0100

    made the sizes a select form

--- src/MenuItemGrid.js
+++ src/MenuItemGrid.js
@@ -56,13 +56,17 @@ export default function MenuItemGrid(props) {
         </div>
 
         <div className="menu-item-col">
-          {props.price.map((item, index) => (
-            <div key={index}>
-              <span className={`menu-item-col`}>
-                {item.size} £{Number(item.price).toFixed(2)}
-              </span>
-            </div>
-          ))}
+          <select>
+            {props.price.map((item, index) => (
+              <option>
+                <div key={index}>
+                  <span className={`menu-item-col`}>
+                    {item.size} £{Number(item.price).toFixed(2)}
+                  </span>
+                </div>
+              </option>
+            ))}
+          </select>
         </div>
       </div>
     </div>
